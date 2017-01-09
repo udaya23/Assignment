0c4b09959d7a2f3141caf34da4c83a08dabc2c9c
swetha23 <udayasree23@gmail.com>
2017-01-08 22:08:00 -0800

    remove deviceHard.xlsx

--- Device42_project/Device42pgm.py
+++ Device42_project/Device42pgm.py
@@ -1,27 +1,17 @@
 import requests
 import certifi
-import time
 import json
 import xlrd
 import sys
-import os
-import json
 import base64
 import collections
 import logging
-from datetime import datetime
+import ast
 from time import strftime
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from ConfigParser import SafeConfigParser
-import ast
 from ast import literal_eval
-import warnings
-import pdb
-import smtplib
-from smtplib import SMTP  
-#from email.mime.text import MIMEText
-#from email.header import Header
-from email.mime.text import MIMEText
+
 
 class Device_d42:
 
@@ -29,11 +19,11 @@ class Device_d42:
             self.read_config()
 
     def log_file(self):
+        """
+        Create file handler which logs even debug messages
+        """
         logger = logging.getLogger('Debugging logs')
         logger.setLevel(logging.INFO)
-        """
-        create file handler which logs even debug messages
-        """
         fh = logging.FileHandler(strftime("mylogfile_%m_%d_%Y.log"))
         logging.debug('This message should go to the log file')
         fh.setLevel(logging.INFO)
@@ -43,19 +33,18 @@ class Device_d42:
         return logger
 
     def read_config(self):
+        """
+        Read all crederntails from a config file
+        """
         self.logger = self.log_file()
         self.logger.info("Reading all the credentials to connect to the database.....")
- #       filepath = sys.argv[1]
-        filepath = "../device_42_project/config_p.cfg"
+        filepath = sys.argv[1] 
         try:
             config = SafeConfigParser()
             config.read(filepath)
             self.username = config.get('config_param', 'D42_USERNAME')
             self.password = config.get('config_param', 'D42_PASSWORD')
             self.url = config.get('config_param', 'D42_URL')
-            """
-            Read values from config file
-            """
             self.update_params = config.get('config_values', 'update_params')
             self.building_params = config.get('config_values', 'building_params')
             self.device_to_rack = config.get('config_values', 'device_to_rack')
@@ -65,12 +54,9 @@ class Device_d42:
             self.params = config.get('config_values', 'params')
             self.device_id = config.get('config_values', 'device_id')
             self.auth = (self.username, self.password)
-            self.get_api = "buildings/"
-            sender = 'udaya.python23@gmail.com'
-            receivers = ['udaya.ackula@gmail.com']
-        except IOError, error:
-            logging.error("error has ocurred in reading values from config file")
-            
+        except IOError,e:
+            self.logger.error(e, exc_info=True)           
+
     def post_data(self, theurl, data):
         method = "POST"
         method = method.upper()
@@ -92,21 +78,21 @@ class Device_d42:
             self.logger.info('Getting data from %s' %theurl)
         except requests.error as err:
             self.logger.error(err)
-
-    def get_names_list(self, url, get_api):
+ 
+    def get_names_list(self, url, api_key):
         """
         Getting the list of building/room/rack names from Device42 API
         """
-        result = self.get_data(url,get_api)
+        result = self.get_data(url,api_key)
         res = ast.literal_eval(result)
-        get_api = get_api[:-1]
-        result = res[get_api]
+        api_key = api_key[:-1]
+        result = res[api_key]
         names = [i['name'] for i in result if 'name' in i]
         return names
 
     def post_building(self, url, building_params):
         """
-        building name is mandatory
+        For POST req to execute Building name parameter is mandatory
         """
         building_params = eval(building_params)
         if building_params.has_key('name') != True:
@@ -119,7 +105,7 @@ class Device_d42:
            
     def post_room(self, url, room_params):
         """
-        building name and room name is required
+        For POST req to execute Building name,room name parameters are required
         """
         buildingnames = self.get_names_list(url, "buildings/")
         for building in buildingnames:
@@ -145,7 +131,7 @@ class Device_d42:
         For POST req to execute rack name, size (u size), room name parameters are required
         check if room doesnt exist re-direct to add room function
         """
-        roomnames = self.check_if_exists(url, "rooms/")
+        roomnames = self.get_names_list(url, "rooms/")
         for room in roomnames:
             if room in rack_params:
                 self.logger.info('Room info exists for device')
@@ -182,11 +168,11 @@ class Device_d42:
 
     def post_device(self, url, params):
         """
-        devices can be added without giving any hardware model
+        Post Devices without giving any hardware model
         """
         params = eval(params)
         theurl = url + "devices/"
-        result = self.post_data_func(theurl, params)
+        result = self.post_data(theurl, params)
         self.logger.info("Device is posted to Device42",json.dumps(result))
  
     def post_device_2_rack(self, url, device_to_rack):
@@ -202,7 +188,7 @@ class Device_d42:
             self.logger.info('Rack info not available for device')           
         else:            
             theurl = url + "device/rack/"
-            result = self.post_data_func(theurl, device_to_rack)
+            result = self.post_data(theurl, device_to_rack)
             self.logger.info("Device is posted to Device42",json.dumps(result))
  
     def update_device(self, url, update_params):
@@ -217,35 +203,34 @@ class Device_d42:
             self.logger.info('Device info has been updated',json.loads(resp.content))
         except requests.HTTPError:
             self.logger.warning('Device id not found')
-
+            
     def check_column_exists(self,lkeys):
         """
         Check if column names are correct"
         """
-        columns_list = ['id','name','asset_no','building','rack','room','orientation','virtual_host','serial_no','hw_model','start_at','live_id','first_added','last_updated','storage_room','discovery_spec','device_host','customer','uuid','service_level','in_service','blade_chassis','fiber_switch','discovery_spec']
+        columns_list = ['id','name','asset_no','type','network_device','in_service','building','rack','room','orientation','virtual_host','serial_no','hw_model','start_at','live_id','first_added','last_updated','storage_room','discovery_spec','device_host','customer','uuid','service_level','blade_chassis','blade_slot_no','device_host_chassis','fiber_switch','discovery_spec']
         for i in range(0,len(lkeys)):
             if lkeys[i] in columns_list:
                 pass
             else:
-                return False
                 self.logger.info("columns names are wrong.Please provide correct column names")
+                sys.exit(1)
         return True
-    
+
     def read_column_names(self,keys):
         """
         Read column headers and check if there is any empty headers
         """
         for i in range(0,len(keys)): 
             if '' in keys:
-                result  = self.send_message()
-                return result
+                result = self.send_message()
                 sys.exit(1)
             else:
-                lkeys = map(str.lower, keys)
+                lkeys = map(str.lower,keys)
                 if self.check_column_exists(lkeys) is True:                   
                     return lkeys
         return True
-
+      
     def build_message(self):
         """
         Build E-mail message and subject
@@ -254,45 +239,53 @@ class Device_d42:
         """
         msgd = MIMEText(msg)
         return msgd
-
+ 
     def send_message(self, sender, receivers):
+        """
+        Send E-mail to Developers incase of error
+        """
         try:
             msg = self.build_message()
+            sender = 'udaya.python23@gmail.com'
+            receivers = ['udaya.ackula@gmail.com']
             s = smtplib.SMTP(host='smtp.gmail.com', port=587)
             s.ehlo()
             s.starttls()
             s.login('udaya.python23@gmail.com','doyoulikewhatyousee')
-#            result = s.sendmail(sender, receivers, msgd.as_string())
             result = s.sendmail(sender, receivers, msg)
             return result
             self.logger.info("Successfully sent email")
             s.quit()
-        except smtplib.SMTPException,error:
+        except smtplib.SMTPException as error:
             self.logger.warning("Error: unable to send email")
             self.logger.warning(str(error))
 
-        
     def read_from_xlsx(self):
+        """
+        Read data from Excel sheet
+        """
         xl_workbook = xlrd.open_workbook("csv_devices.xlsx")
         sheet = xl_workbook.sheet_by_index(0)
         keys = [str(sheet.cell(0, col_index).value) for col_index in xrange(sheet.ncols)]
-        keys_s = self.read_column_names(keys)
-        if not self.read_column_names(keys):
-            return False
+        lkeys = self.read_column_names(keys)
         dict_list = []
 #        num_rows = sheet.nrows-1
         for row_index in xrange(1, sheet.nrows):
-            d = {keys_s[col_index]: str(sheet.cell(row_index, col_index).value) 
+            d = {lkeys[col_index]: str(sheet.cell(row_index, col_index).value) 
                 for col_index in xrange(sheet.ncols)}
             dict_list.append(d)
         return dict_list
+    
 
     def post_multipledata(self, url):
+        """
+        Post Request for multiple data excel sheet
+        """
         theurl = url + "device/"
         headers = {'Content-type': 'application/x-www-form-urlencoded',\
     			'Authorization' : 'Basic '+ base64.b64encode(self.username + ':' + self.password)}
         data_s = []
-        data_s,num_rows = self.read_from_xlsx()
+        data_s = self.read_from_xlsx()
         data_len = len(data_s)-1
         try:
             for i in range(0,data_len):
@@ -336,7 +329,7 @@ class Device_d42:
             self.logger.error(err)
 
 c = Device_d42()
-c.get_names_list(c.url,c.get_api)
+#c.check_if_exists(c.url,c.api_key)
 #c.post_racks(c.url, c.rack_params)
 #c.get_data(c.url, c.get_api)
 #c.post_device(c.url, c.params)
---
+++ Device42_project/testcases.py
@@ -0,0 +1,125 @@
+import mock
+import unittest
+import requests
+import smtplib
+import os
+from latest_cfglogg import *
+from testfixtures import LogCapture
+from test_module import Device_d42
+from mock import patch, call
+
+"""
+def resolve_file(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+FILE_EXCEL = resolve_file('../device_42_project/deviceHard.xlsx')
+"""
+
+class ClientTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.LOCAL_INSTALL_DIR = os.path.join("../device_42_project/","config_p.cfg")
+        self.client = Device_d42()
+        self.room_params = {"building" : "New-Haven-DC","name": "DC room3","notes" : "Third room"}
+
+
+    def test_check_column_exists(self):
+        test_keys = ['id','buildins']
+        actual_result = self.client.check_column_exists(test_keys)
+        self.assertEqual(actual_result,False)
+                
+
+    def test_get_names_list(self):
+        url = 'https://192.168.0.30/api/1.0/'
+        get_api = "buildings/"
+        expected_resp = ['New-Haven-DC', 'Elcamino-Drive-CA']
+        actual_result = self.client.get_names_list(url,get_api)       
+        self.assertEqual(actual_result,expected_resp)
+
+    def test_for_file_exists(self):
+        self.assertEqual(os.path.exists(self.LOCAL_INSTALL_DIR),True)
+
+    def test_logger_messages(self):       
+        with LogCapture() as l:
+          logger_test = c.log_file()
+          logger_test.info('This message is tested from test case for checking if log messages are written')
+
+    def test_local_excel(self):
+#       Test reading from a local Excel file.And chck further if all rows are read or not
+        xl_workbook = xlrd.open_workbook("csv_devices.xlsx")
+        self.assertTrue(xl_workbook)
+        sheet = xl_workbook.sheet_by_index(0)
+        num_rows = sheet.nrows
+        self.assertEqual(num_rows,14)
+              
+
+    # Mock 'smtplib.SMTP' class
+    @patch("smtplib.SMTP")
+    def test_send_email(self, mock_smtp):
+        # Check the E-mail Build test message and send message functions of MyAPIClient.
+        from_address = 'udaya.python23@gmail.com'
+        to_address = ['udaasre23@gmail.com']
+        # Returns a send failur for the first recipient
+        error = {
+            to_address[0]: 
+                (450, "Requested mail action not taken: mailbox unavailable")
+        }
+        msg = "Test Message"
+ 
+        # Get instance of mocked SMTP object
+        instance = mock_smtp.return_value
+        instance.sendmail.return_value = error
+        
+         # Call 'send_message' function            
+        result = self.client.send_message(from_address, to_address)
+        
+        #check if the function is called exactly once
+        self.assertEqual(instance.sendmail.call_count, 1)
+
+        #check if function returns error unable to send e-mail
+        self.assertEqual(instance.sendmail.return_value, error)
+
+
+    @mock.patch('test_module.requests.get')
+    def test_get_ok(self, mock_get):
+        
+#        Test getting a 200 OK response from the get method of MyAPIClient.
+#        Construct our mock response object, giving it relevant expected
+#        behaviours
+        
+        mock_response = mock.Mock()
+        expected_status = 200
+        mock_response.return_value = expected_status
+
+        # Assign our mock response as the result of our patched function
+        mock_get.return_value = mock_response
+
+        url = 'https://192.168.0.30/api/1.0/'
+        get_api = "rooms/"
+        response_dict = self.client.get_data(url=url,get_api=get_api)
+        
+        # If we want, we can check the contents of the response
+        self.assertEqual(response_dict, expected_status)
+
+    @mock.patch('test_module.requests.post')
+    def test_post_ok(self, mock_post):       
+#        Test getting a 200 OK response from the post method of MyAPIClient.
+#        Construct our mock response object, giving it relevant expected
+#        behaviours
+        mock_response = mock.Mock()
+        expected_status = 200
+        mock_response.return_value = expected_status
+
+        # Assign our mock response as the result of our patched function
+        mock_post.return_value = mock_response
+
+        url = 'https://192.168.0.30/api/1.0/rooms/'
+        response_dict = self.client.post_data_func(data = self.room_params, theurl=url)
+        # If we want, we can check the contents of the response
+        self.assertEqual(response_dict, expected_status)
+                
+    
+if __name__ == "__main__":
+    unittest.main()
+
+
