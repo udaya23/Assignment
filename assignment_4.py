1f07a0fa5bdb2c330de0ff93fd9eeca9c75744b8
swetha23 <udayasree23@gmail.com>
2017-01-27 22:07:22 -0800

    re

--- assignment_4.py
+++
@@ -1,8 +0,0 @@
-#Given a collection of numbers, return all possible permutations.
-n = [1,2,3]
-
-for i in n:
-    for j in n:
-        for k in n:
-            if i != j and j !=k and k != i:
-                print [i,j,k]
